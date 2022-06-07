6dd87c410e3dfefdaa0a43a4440da5055ee8f186
Colby Soldan <colbylsoldan@gmail.com>
2022-06-07 09:43:00 -0500

    first commit

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static string Add(int x, int y, bool dollars)
+        {
+
+            if (dollars)
+            {
+                return $"{x + y} dollars";
+            }
+            else
+            {
+                return $"{x + y} dollar";
+            }
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            //calls first Add method
+            Console.WriteLine(Add(3, 4)); //7
+
+            //calls second Add method
+            Console.WriteLine(Add(3.543, 634.734)); //638.277
+
+            //calls third Add method
+            Console.WriteLine(Add(4, 6, true)); //10 dollars
+
+            Console.WriteLine(Add(1, 0, false)); // 1 dollar
+
+            Console.ReadLine();
+        }
+    }
+}
+
